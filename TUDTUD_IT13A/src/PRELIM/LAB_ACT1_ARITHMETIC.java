8722142922c194143e7244b336cd978f484ae2eb
Arriel Denver T. Cadavoz <tudtudleanne@gmail.com>
2025-02-13 18:17:11 +0800

    "Added HelloWorld class"

---
+++ TUDTUD_IT13A/src/PRELIM/LAB_ACT1_ARITHMETIC.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PRELIM;
+
+/**
+ *
+ * @author yielc
+ */
+public class LAB_ACT1_ARITHMETIC {
+    public static void main(String[] args){
+        int f = 10;
+        int g = 4;
+        int h = 6;
+        int i = 3;
+        
+        int fff = f * g + h;
+        int ggg = (f - g) % h;
+        int hhh = (f + g + h) / i;
+        int iii = f * h - (g * g);
+        
+        System.out.println("10 * 4 + 6 = " + fff);
+        System.out.println("10 - 4 % 6 = " + ggg);
+        System.out.println("10 + 4 + 6 / 3 = " + hhh);
+        System.out.println("10 * 6 - 4 * 4 = " + iii);
+        
+                
+                
+    }
+}
