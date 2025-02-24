0fb4f13d19c5c4db3b6400c8a702c8816d0dbec9
Arriel Denver T. Cadavoz <tudtudleanne@gmail.com>
2025-02-24 15:53:36 +0800

    Added Java Program Greatest Number

--- IT13A_Tudtud/src/prelim/Lab2_Act2_Greatest_Number.java
+++ IT13A_Tudtud/src/prelim/Lab2_Act2_Greatest_Number.java
@@ -4,10 +4,31 @@
  */
 package prelim;
 
+import java.util.Scanner;
+
 /**
  *
  * @author yielc
  */
 public class Lab2_Act2_Greatest_Number {
-    
+    public static void main(String[] args){
+        int l, g, t;
+        
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Input the 1st number: ");
+        l = in.nextInt();
+        System.out.println("Input the 2nd number: ");
+        g = in.nextInt();
+        System.out.println("Input the 3rd number: ");
+        t = in.nextInt();
+        
+        if (l > g && l > t){
+            System.out.println("l is the greatest number");
+        } else if (g > l && g > t){
+            System.out.println("g is the greatest number");
+        } else if (t > l && t > g){
+            System.out.println("t is the greatest number");
+        }
+    }
 }
