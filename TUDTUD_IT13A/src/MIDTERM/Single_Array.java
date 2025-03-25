b726df9a3c77b92e58181f51dca4a0b46576f0ce
Leanne <tudtudleanne@gmail.com>
2025-03-25 11:14:42 +0800

    Add files via upload

---
+++ TUDTUD_IT13A/src/MIDTERM/Single_Array.java
@@ -0,0 +1,14 @@
+
+package MIDTERM;
+
+public class Single_Array {
+     public static void main(String[] args) {
+        
+        char letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+        
+        for (char i = 0; i < letters.length; i++) {
+            System.out.print(letters[i] + ", ");
+        }
+    }
+ 
+}
